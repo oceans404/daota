5411cf6be7c293ea27642f4dd902460cbdd221c1
Steph <so.orpilla@gmail.com>
2022-04-23 12:24:36 +0200

    Implement WalletConnect for wallet agnostic dapp connection

--- daota-frontend/README.md
+++ daota-frontend/README.md
@@ -8,15 +8,7 @@ In the project directory, you can run:
 
 ### `npm run dev`
 
-Runs the app in the development mode with hot reloading for tailwind (nodemon).\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm start`
-
-Runs the app in the development mode.\
+Runs the app in the development mode with hot reloading for Tailwind CSS (nodemon).\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
@@ -37,42 +29,10 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+## Sponsored Tech
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+### WalletConnect
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+[WalletConnect Docs](https://docs.walletconnect.com/)
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+We're using WalletConnect as a wallet agnostic connection to a user's address.
--- daota-frontend/src/App.js
+++ daota-frontend/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import WalletConnect from '@walletconnect/client';
+import QRCodeModal from '@walletconnect/qrcode-modal';
 import Container from './components/Container';
 
 import logo from './logo.svg';
@@ -12,42 +14,99 @@ const AppText = {
   about: 'About',
 };
 
+const connector = new WalletConnect({
+  bridge: 'https://bridge.walletconnect.org', // Required
+  qrcodeModal: QRCodeModal,
+});
+
 export default function App() {
+  const [walletAccount, setWalletAccount] = useState(null);
+
+  useEffect(() => {
+    if (connector?.connected) {
+      setWalletAccount(connector?.accounts && connector.accounts[0]);
+    }
+  }, []);
+
+  const handleConnectWallet = () => {
+    if (!connector.connected) {
+      connector.createSession();
+      connector.on('connect', (error, payload) => {
+        if (error) {
+          throw error;
+        }
+
+        // Get provided accounts and chainId
+        const { accounts, chainId } = payload.params[0];
+        setWalletAccount(accounts && accounts[0]);
+      });
+
+      connector.on('session_update', (error, payload) => {
+        if (error) {
+          throw error;
+        }
+
+        // Get updated accounts and chainId
+        const { accounts, chainId } = payload.params[0];
+        setWalletAccount(accounts && accounts[0]);
+      });
+
+      connector.on('disconnect', (error, payload) => {
+        if (error) {
+          throw error;
+        }
+        setWalletAccount(null);
+      });
+    }
+  };
+
   return (
     <Router>
       <div>
-        <nav class='font-sans sm:text-left  bg-white shadow sm:items-baseline w-full'>
+        <nav className='font-sans sm:text-left  bg-white shadow sm:items-baseline w-full'>
           <Container addClasses='flex flex-col text-center sm:flex-row sm:justify-between'>
-            <div class='mb-2 sm:mb-0'>
-              <div class='text-2xl no-underline text-grey-darkest hover:text-blue-dark'>
+            <div className='mb-2 sm:mb-0'>
+              <div className='text-2xl no-underline text-grey-darkest hover:text-blue-dark'>
                 <Link to='/'>{AppText.projectName}</Link>
               </div>
             </div>
             <div>
-              <a
-                href='/one'
-                class='text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2'
-              >
+              {/* <div className='text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2'>
                 <Link to='/about'>{AppText.about}</Link>
-              </a>
+              </div> */}
+              <div className='text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2'>
+                {walletAccount ? (
+                  <span>
+                    Connected as {walletAccount.substring(0, 6)}...
+                    {walletAccount.substring(38)}
+                  </span>
+                ) : (
+                  <button onClick={handleConnectWallet}>Connect Wallet</button>
+                )}
+              </div>
             </div>
           </Container>
         </nav>
 
         <Routes>
           <Route path='/about' element={<About />} />
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home walletInfo={{ walletAccount }} />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-function Home() {
+function Home(props) {
+  const {
+    walletInfo: { walletAccount = null },
+  } = props;
   return (
     <Container>
       <div className='bg-[blue]'>
-        <h2>{AppText.projectName}</h2>
+        <h2>
+          Connected as {walletAccount || ''} to {AppText.projectName}
+        </h2>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
