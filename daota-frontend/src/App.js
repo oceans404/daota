82f3805f6aace7f9dfba063f309b5263c314d402
Steph <so.orpilla@gmail.com>
2022-04-23 11:10:42 +0200

    Implement simple navbar

--- daota-frontend/src/App.js
+++ daota-frontend/src/App.js
@@ -15,15 +15,21 @@ export default function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to='/'>{AppText.projectName}</Link>
-          </li>
-          <li>
-            <Link to='/about'>{AppText.about}</Link>
-          </li>
-        </ul>
-
+        <nav class='font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full'>
+          <div class='mb-2 sm:mb-0'>
+            <div class='text-2xl no-underline text-grey-darkest hover:text-blue-dark'>
+              <Link to='/'>{AppText.projectName}</Link>
+            </div>
+          </div>
+          <div>
+            <a
+              href='/one'
+              class='text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2'
+            >
+              <Link to='/about'>{AppText.about}</Link>
+            </a>
+          </div>
+        </nav>
         <Routes>
           <Route path='/about' element={<About />} />
           <Route path='/' element={<Home />} />
