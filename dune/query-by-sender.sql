daa7439908eb294c03ce1066033fcceaddadd7d7
Christina Polyukh <christinapolyukh@Christinas-MacBook-Air.local>
2022-04-24 05:25:58 +0200

    Update Dune query

--- dune/query-by-sender.sql
+++ dune/query-by-sender.sql
@@ -1,9 +1,10 @@
 with tokenTransfers as (
-    select contract_address, "from", "to", count(*)
+    select contract_address, "from", "to", count(*) as c
     FROM erc20."ERC20_evt_Transfer" tr
     where "contract_address" ='\x35bd01fc9d6d5d81ca9e055db88dc49aa2c699a8'
     group by "from", "contract_address", "to"
 )
 
 select * from tokenTransfers
-where "from"='\x00000000003b3cc22af3ae1eac0440bcee416b40'
+where "from"='\x74de5d4fcbf63e00296fd95d33236b9794016631'
+and c > 5
